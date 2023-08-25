be8a5bb0c36fd48e4c1ce85e2f353e3a92be6e6f
vasilpichurov <77060272+vasilpichurov@users.noreply.github.com>
2023-08-25 14:05:52 +0300

    commit project

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1aa8457bdfmshe1547fe960a9202p1c8b3ejsnd2b509a0810f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "444ddbf18c0745bc9f3728c7201e005b"
